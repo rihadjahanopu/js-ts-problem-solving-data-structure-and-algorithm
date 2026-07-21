b0fc4e53490764607b655e2856fac819861dee78
Rihad Jahan Opu <96970767+rihadjahanopu@users.noreply.github.com>
2026-07-21 12:25:17 +0600

    🚧 WIP: typescript new solve problem 21/07/26

---
+++ typescript-practice/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nui = (a, b) => {
+    const result = a / b;
+    return result;
+};
+console.log(nui(26, 56));
+let subtitle = undefined;
+console.log(subtitle);
+const nab = 20;
+console.log(nab);
+const ms = Math.random() * nab;
+console.log(ms);
+//# sourceMappingURL=index.js.map
--- typescript-practice/index.ts
+++ typescript-practice/index.ts
@@ -1,3 +1,39 @@
-const nui = (a: number, b: number) => {
+const nui = (a: number, b: number): number => {
 	return a / b;
 };
+
+let mult = nui(26, 46);
+
+console.log(mult);
+
+console.log(nui(26, 56));
+
+let subtitle: string | undefined = undefined;
+
+console.log(subtitle);
+
+const nab: number = 20;
+
+console.log(nab);
+
+const ms: number = Math.random() * nab;
+
+console.log(ms);
+
+type User = {
+	id: number;
+	name: string;
+	email: string;
+	role?: string;
+	readonly createAt: Date;
+};
+
+const userOne: User = {
+	id: 1,
+	name: "Rihad",
+	email: "r@gmail.com",
+	createAt: new Date(),
+};
+
+// userOne.createAt = new Date();
+const me = 2;
